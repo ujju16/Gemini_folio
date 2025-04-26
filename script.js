b626fe1c1ebdaacbbdab705dd04b1d80e3ec7c40
ujju16 <jul.darrouza@gmail.com>
2025-04-26 02:39:56 +0200

    Initial Commit

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+// Vous pouvez ajouter ici du code JavaScript pour des interactions plus avancées
+// comme des animations au scroll, la validation du formulaire de contact, etc.
+
+console.log("Portfolio script chargé !");
